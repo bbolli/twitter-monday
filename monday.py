1626fc6c34580c9b7272a3da2f2e6a11f45b4105
Beat Bolli <bbolli@ewanet.ch>
2014-07-27 14:23:34 +0200

    Add the Twitter API calls
    This requires the Twitter library from https://github.com/sixohsix/twitter

--- monday.py
+++ monday.py
@@ -5,10 +5,22 @@
 from __future__ import print_function
 
 from datetime import datetime, timedelta
+from email.utils import parsedate_tz, mktime_tz
 import errno
 import operator
 import os
 import sys
+import time
+
+from twitter.api import Twitter, TwitterError
+from twitter.oauth import OAuth, write_token_file, read_token_file
+from twitter.oauth_dance import oauth_dance
+
+
+# Registered by @swissbolli
+CONSUMER_KEY = '2fLClfUjnO720IiTSZXwxiQM6'
+CONSUMER_SECRET = 'uHLl38PJy1clObRCWHkjRy3nP3h0km7LLTXSiXMRF9ExBUjBVF'
+OAUTH_FILENAME = os.environ.get('HOME', os.environ.get('USERPROFILE', '.')) + os.sep + '.twitter_monday_oauth'
 
 ### date handling ###
 
@@ -29,6 +41,45 @@ def sunday_after(dt, offset=1):
     return s
 
 
+### Twitter API ###
+
+def get_twitter_api():
+    """If the user is not authorized yet, do the OAuth dance and save the
+    credentials in her home folder for future incovations.
+    Then read the credentials and return the authorized Twitter API object."""
+    if not os.path.exists(OAUTH_FILENAME):
+        oauth_dance("@swissbolli's Monday Twitter Backup",
+            CONSUMER_KEY, CONSUMER_SECRET, OAUTH_FILENAME
+        )
+    oauth_token, oauth_token_secret = read_token_file(OAUTH_FILENAME)
+    return Twitter(
+        auth=OAuth(oauth_token, oauth_token_secret, CONSUMER_KEY, CONSUMER_SECRET),
+        domain='api.twitter.com'
+    )
+
+def get_tweets(twitter, **args):
+    """Read tweets from the authenticated user's timeline one at a time,
+    starting with the most recent one."""
+    kwargs = {'count': 500}
+    kwargs.update(args)
+    exc_count = 0
+    while True:
+        try:
+            tweets = twitter.statuses.user_timeline(**kwargs)
+        except TwitterError as e:
+            print(e, file=sys.stderr)
+            exc_count += 1
+            if exc_count >= 10:
+                break
+            time.sleep(3)
+            continue
+        if not tweets:
+            break
+        for tweet in tweets:
+            yield tweet
+            kwargs['max_id'] = tweet['id'] - 1
+
+
 ### getting a week's worth of tweets ###
 
 class Week:
@@ -37,25 +88,23 @@ class Week:
     def __init__(self, mid_week):
         latest = sunday_after(mid_week, 1)
         earliest = sunday_after(mid_week, -1)
-        """r = Reader.new
-        @tweets = []
-        while true do
-          tweet = r.next
-          break if tweet.time <= earliest
-          @tweets << tweet if tweet.time <= latest
-        end"""
-        class MockedTweet:
-            def __init__(self, nr):
-                self.time = latest - timedelta(days=nr, hours=nr * 2, minutes=nr * 3)
-            def __repr__(self):
-                return "\nMockedTweet from %s" % self.time
-        self.tweets = [MockedTweet(1), MockedTweet(2), MockedTweet(3), MockedTweet(4)]
-        self.tweets.sort(key=operator.attrgetter('time'))
+        twitter = get_twitter_api()
+        user = twitter.account.settings(_method='GET')
+        self.tweets = []
+        for tweet in get_tweets(twitter, screen_name=user['screen_name']):
+            tm = tweet['_time'] = datetime.fromtimestamp(
+                mktime_tz(parsedate_tz(tweet['created_at']))
+            )
+            if tm <= earliest:
+                break
+            if tm <= latest:
+                self.tweets.append(tweet)
+        self.tweets.sort(key=operator.itemgetter('_time'))
 
     @property
     def sunday(self):
         if self.tweets:
-            return sunday_after(self.tweets[0].time)
+            return sunday_after(self.tweets[0]['_time'])
 
 
 ### formatting the tweets ###
